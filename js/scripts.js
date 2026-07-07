92d42e6fec9a29fa2c4b80d1e7a924c57455afe4
Korochkin Stepan <stepa.stepa444@gmail.com>
2026-07-07 21:50:27 +0300

    d

--- js/scripts.js
+++ js/scripts.js
@@ -197,7 +197,7 @@ $(document).ready(function () {
             end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '\u041a\u0430\u0444\u0435 \u0423\u0434\u0430\u0447\u0430, 55.92833576466846, 41.12000206544185',
 
             // Event Description
             description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx9xXBUGso7TeZ0ENAhzDOCPcol-4f1ocF0lAMYUBwmtlG4oA-B6caVt5jf-tofxXim/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 55.92833576466846, lng: 41.12000206544185};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
